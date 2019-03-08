e6eef4e1107936eb9358edf20adb94c019291115
Henry <henry@forbescrypto.com>
2019-03-08 17:54:26 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+import sys
+sys.path.append('/usr/local/lib/python2.7/site-packages')
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+
+	return render_template("index.html")
+
+
+if __name__ == "__main__":
+
+	app.run(host = '0.0.0.0', port = 8080, threaded=True)
