f0bfd87afaadf42df6abdb4ebef530adbd546e7b
Henry <henry@forbescrypto.com>
2019-03-09 10:17:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def index():
 @app.route('/postreceive', methods=['POST'])
 def on_push():
 	data = request.form
-	print('New commit received: ', data)
+	print('New commit received: ', data.get('payload')['commits']['id'])
 	g = git.cmd.Git(os.getcwd())
 	g.pull()
 
