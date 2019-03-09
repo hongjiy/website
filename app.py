025ddda8b3a50c82da896dd06a0a259141c67b4f
Henry <henry@forbescrypto.com>
2019-03-09 10:43:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -17,8 +17,9 @@ def on_push():
 	print('New commit received: ', data['head_commit']['id'])
 	g = git.cmd.Git(os.getcwd())
 	g.pull()
-
+	print('codebase update: successful')
 	return json.dumps(('', 200))
+
 
 if __name__ == "__main__":
 	app.config['TEMPLATES_AUTO_RELOAD'] = True
