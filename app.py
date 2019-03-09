021ec3bf508312011aecb31e9b25dc3ef22e3ba9
Henry <henry@forbescrypto.com>
2019-03-09 09:52:44 -0500

    added git support

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template
-from github_webhook import Webhook
+from flask import Flask, render_template, request
+import git
+import os
 
 app = Flask(__name__)
-#webhook = Webhook(app)
 
 
 @app.route('/')
@@ -10,14 +10,15 @@ def index():
 	return render_template('index.html')
 
 
-# @webhook.hook()
-# def on_push(data):
-# 	print("Got push with: {0}".format(data))
-
 @app.route('/postreceive', methods=['POST'])
 def on_push():
-	print('got note')
+	data = request.form
+	print(data)
+	g = git.cmd.Git(os.getcwd())
+	g.pull()
 
+
+	return 200
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=80, threaded=True, debug=False)
